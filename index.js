623c8d206842a1ccbf7e1ab611e350ec9ebf31f7
sergio <sergiod.roddj@gmail.com>
2023-03-05 00:50:27 -0400

    Made the website

---
+++ index.js
@@ -0,0 +1,11 @@
+function find_out() {
+    const div = document.body.querySelector("div")
+    const text = document.querySelector("h2")
+    text.innerText = "no they don't love you sorry"
+    const button = document.querySelector("button")
+    button.innerText = "retry?"
+    button.onclick = () => {
+        text.innerText = "you cant get any other response. it's like, not coded in. they just don't love u bro"
+        button.remove()
+    }
+}
